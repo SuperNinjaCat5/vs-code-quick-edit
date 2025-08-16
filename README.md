d248957fb01aea999666c10ed5a8aa2b83398f8b
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-08-16 10:41:07 -0400

    updated it

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ in vs code. Just type `:Vscode` or `:Vscodefolder`! It's that easy!
 
 # Installing
 
-### Prereqrisites
+## Prereqrisites
 
 1. Be sure to have vs code installed and working
 2. Make sure to have neovim setup (if you dont know how use the offical kickstart repo)
@@ -19,3 +19,19 @@ in vs code. Just type `:Vscode` or `:Vscodefolder`! It's that easy!
 
 > Test the command `code .` and `nvim` in terminal to be sure everything will work
 
+## Setup
+
+To setup you will first need to clone the repo into a folder of your choice. I reccomend the one that has your other plugins.
+We will be using lazy.nvim to install the plugin.
+
+1. In your lazy file (~/.config/nvim/lua/custom/plugins/init.lua if you did the kickstart project) paste this code:
+{
+  dir = vim.fn.expand('~/.config/nvim/lua/vs-code-quick-edit.nvim'),
+  config = function()
+    require('vs-code-quick-edit').setup {
+      file_keymap = '<Leader>o',
+      folder_keymap = '<Leader>O',
+    }
+  end,
+}
+> It may need to be modified
--- lua/vs-code-quick-edit.lua
+++ lua/vs-code-quick-edit.lua
@@ -1,9 +1,14 @@
 print 'loaded vs-code-quick-edit.nvim'
 local M = {}
 
-function M.vscode()
+M.opts = { -- Keybinds defults
+  file_keymap = '<Leader>v', --defult <Leader>v
+  folder_keymap = '<Leader>V', --defult <Leader>V
+}
+
+function M.vscode(filepath)
   print 'Opening Current File in VS code.'
-  local file = vim.fn.expand '%:p'
+  local file = filepath or vim.fn.expand '%:p'
   if file == '' then
     print 'No file to open!'
     return
@@ -12,19 +17,29 @@ function M.vscode()
   vim.fn.jobstart({ 'code', '-n', file }, { detach = true })
 end
 
-function M.vscode_folder()
-  local folder = vim.fn.expand '%:p:h'
+function M.vscode_folder(dirpath)
+  local folder = dirpath or vim.fn.expand '%:p:h'
   if folder == '' then
     print 'No folder to open!'
     return
   end
-  print 'Opening Current File in VS code.'
+  print 'Opening Current Folder in VS code.'
   vim.fn.jobstart({ 'code', '-n', folder }, { detach = true })
 end
 
-vim.api.nvim_command 'command! Vscode lua require("vs-code-quick-edit").vscode()'
-vim.api.nvim_command 'command! Vscodefolder lua require("vs-code-quick-edit").vscode_folder()'
-vim.keymap.set('n', '<Leader>v', ':Vscode<CR>', { noremap = true, silent = true }) -- Shortcut to open file in vs code (defult <Leader>v)
-vim.keymap.set('n', '<Leader>V', ':Vscodefolder<CR>', { noremap = true, silent = true }) -- Shortcut to open folder in vs code (defult <Leader>V)
+function M.setup(opts)
+  M.opts = vim.tbl_extend('force', M.opts, opts or {})
+
+  vim.api.nvim_command [[
+  command! -nargs=? -complete=file Vscode lua require("vs-code-quick-edit").vscode(<f-args>)
+  ]]
+
+  vim.api.nvim_command [[
+  command! -nargs=? -complete=dir Vscodefolder lua require("vs-code-quick-edit").vscode_folder(<f-args>)
+  ]]
+
+  vim.keymap.set('n', M.opts.file_keymap, ':Vscode<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', M.opts.folder_keymap, ':VscodeFolder<CR>', { noremap = true, silent = true })
+end
 
 return M
