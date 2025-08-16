aa997c83cfd954dc38e4bcd24a48fabfc5db9d31
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-08-16 07:39:59 -0400

    Updated

--- lua/vs-code-quick-edit.lua
+++ lua/vs-code-quick-edit.lua
@@ -1,3 +1,4 @@
+print 'loaded vs-code-quick-edit.nvim'
 local M = {}
 
 function M.hello_world()
@@ -5,6 +6,6 @@ function M.hello_world()
 end
 
 -- Map a command to the function
-vim.api.nvim_command 'command! HelloWorld lua require("myplugin").hello_world()'
+vim.api.nvim_command 'command! HelloWorld lua require("vs-code-quick-edit").hello_world()'
 
 return M
